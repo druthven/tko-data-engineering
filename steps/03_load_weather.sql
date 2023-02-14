a7081b859fc4256222ebbaf390f5669d46d16aa2
David Ruthven <david.ruthven@snowflake.com>
2023-02-14 11:25:07 +0000

    Update UDF

--- steps/03_load_weather.sql
+++ steps/03_load_weather.sql
@@ -9,8 +9,8 @@ Last Updated: 1/9/2023
 -- SNOWFLAKE ADVANTAGE: Visual Studio Code Snowflake native extension (PrPr, Git integration)
 
 
-USE ROLE HOL_ROLE;
-USE WAREHOUSE HOL_WH;
+USE ROLE HOL_DE_ROLE;
+USE WAREHOUSE HOL_DE_WH;
 
 
 -- ----------------------------------------------------------------------------
